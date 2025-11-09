1ce66357fdefeeb498edacccb69a1afa71965229
hygorrasec <hygorrasec@gmail.com>
2025-11-09 12:29:51 -0300

    Exemplos práticos de useEffect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import Batalha from "./components/Batalha"
+import Exemplo1 from "./components/Exemplo1"
+import Exemplo2 from "./components/Exemplo2"
+import Exemplo3 from "./components/Exemplo3"
 
 function App() {
-  return <Batalha />
+  return (
+    <>
+      <Exemplo1 />
+      <Exemplo2 />
+      <Exemplo3 />
+    </>
+  )
 }
 
 export default App
